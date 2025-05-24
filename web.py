2516162df62f911f1e68d89ec7f94ccea8f0a5a9
Non <ch.non_st@gmail.com>
2025-05-25 02:03:23 +0700

    add Information Dashboard Display in Website

--- web.py
+++ web.py
@@ -4,10 +4,14 @@ import data as data
 st.set_page_config(layout="wide") 
 
 row1 = st.columns([1,10,1])
+
 with row1[1]:
+
     row1_sub = st.columns(6)
+
     with row1_sub[0]:
         st.title("MAJOR")
+
     with row1_sub[1]:
         st.header(data.df["ราคาปิด"][0] )
         change = str(data.df["เปลี่ยนแปลง"][0]) + " (" + str(data.df["เปลี่ยนแปลง(%)"][0]) + "%)"
@@ -15,6 +19,7 @@ with row1[1]:
             st.success(change)
         else:
             st.error(change)
+
     with row1_sub[3]:
         st.error("lowest : " + str(data.df["ราคาต่ำสุด"][0]))
         st.success("hightest : " + str(data.df["ราคาสูงสุด"][0]))
@@ -26,12 +31,15 @@ with row1[1]:
     with row1_sub[5]:
         st.write("Value (Million Baht)")
         st.subheader(str(data.df["มูลค่า(ล้านบาท)"][0]))
+
 row2 = st.columns([1,4,1])
+
 with row2[1]:
     month = st.radio("Month(s)",[1,3,6], index=2, horizontal=True)
     st.pyplot(data.graph(month))
 
 row3 = st.columns(1)
+
 with row3[0]:
     st.write("Data of " + str(month) + " Months")
     st.dataframe(data.table(month), use_container_width=True)
